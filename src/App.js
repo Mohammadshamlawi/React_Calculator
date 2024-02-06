f410147d10411cbcd706123804bbfc74fe610722
Mohammadshamlawi <Mohammad@shamlawi.com>
2024-02-06 14:55:37 +0300

    Better UI. Added mod and back operations.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 function NumberButton({ value, onClick }) {
@@ -27,11 +29,23 @@ function OperatorButton({ operation, onClick }) {
 
 function App() {
     const [value, updateValue] = useState("");
+    const operations = ["+", "-", "\u00d7", "÷", "%", "."];
 
     function handleButtonClick(num) {
-        value === "undefined"
-            ? updateValue(num)
-            : updateValue(value + num.toString());
+        let val = "0";
+        if (num === "back") {
+            if (value !== "undefined" && value !== "0" && value.length > 0)
+                val = value.substring(0, value.length - 1);
+        } else if (
+            value === "undefined" ||
+            (value === "0" && !operations.includes(num))
+        ) {
+            val = num;
+        } else {
+            val = value + num.toString();
+        }
+
+        updateValue(val);
     }
 
     function printResult() {
@@ -48,86 +62,101 @@ function App() {
         } catch (error) {
             val = "undefined";
         }
+
+        val = val === "" ? "0" : val;
+
         updateValue(val);
     }
 
     function clear() {
-        updateValue("");
+        updateValue("0");
     }
 
     return (
         <div className="App">
             <header className="App-header">
+                <div className="output">{value}</div>
                 <div className="calculator">
-                    <div className="calculator__output">{value}</div>
-                    <div className="calculator__keys">
-                        <OperatorButton
-                            operation={"+"}
-                            onClick={() => handleButtonClick("+")}
-                        />
-                        <OperatorButton
-                            operation={"-"}
-                            onClick={() => handleButtonClick("-")}
-                        />
-                        <OperatorButton
-                            operation={"\u00d7"}
-                            onClick={() => handleButtonClick("\u00d7")}
-                        />
-                        <OperatorButton
-                            operation={"÷"}
-                            onClick={() => handleButtonClick("÷")}
-                        />
-                        <NumberButton
-                            value={7}
-                            onClick={() => handleButtonClick("7")}
-                        />
-                        <NumberButton
-                            value={8}
-                            onClick={() => handleButtonClick("8")}
-                        />
-                        <NumberButton
-                            value={9}
-                            onClick={() => handleButtonClick("9")}
-                        />
-                        <NumberButton
-                            value={4}
-                            onClick={() => handleButtonClick("4")}
-                        />
-                        <NumberButton
-                            value={5}
-                            onClick={() => handleButtonClick("5")}
-                        />
-                        <NumberButton
-                            value={6}
-                            onClick={() => handleButtonClick("6")}
-                        />
-                        <NumberButton
-                            value={1}
-                            onClick={() => handleButtonClick("1")}
-                        />
-                        <NumberButton
-                            value={2}
-                            onClick={() => handleButtonClick("2")}
-                        />
-                        <NumberButton
-                            value={3}
-                            onClick={() => handleButtonClick("3")}
-                        />
-                        <NumberButton
-                            value={"."}
-                            onClick={() => handleButtonClick(".")}
-                        />
-                        <NumberButton
-                            value={0}
-                            onClick={() => handleButtonClick("0")}
-                        />
-                        <NumberButton value={"C"} onClick={clear} />
-                        <button
-                            className="calculator__key calculator__key--enter"
-                            onClick={printResult}
-                        >
-                            =
-                        </button>
+                    <div className="center_calc">
+                        <div className="calculator__keys">
+                            <OperatorButton operation={"C"} onClick={clear} />
+                            <OperatorButton
+                                operation={"\u00d7"}
+                                onClick={() => handleButtonClick("\u00d7")}
+                            />
+                            <OperatorButton
+                                operation={"÷"}
+                                onClick={() => handleButtonClick("÷")}
+                            />
+                            <OperatorButton
+                                operation={
+                                    <FontAwesomeIcon icon={faDeleteLeft} />
+                                }
+                                onClick={() => handleButtonClick("back")}
+                            />
+                            <NumberButton
+                                value={7}
+                                onClick={() => handleButtonClick("7")}
+                            />
+                            <NumberButton
+                                value={8}
+                                onClick={() => handleButtonClick("8")}
+                            />
+                            <NumberButton
+                                value={9}
+                                onClick={() => handleButtonClick("9")}
+                            />
+                            <OperatorButton
+                                operation={"+"}
+                                onClick={() => handleButtonClick("+")}
+                            />
+                            <NumberButton
+                                value={4}
+                                onClick={() => handleButtonClick("4")}
+                            />
+                            <NumberButton
+                                value={5}
+                                onClick={() => handleButtonClick("5")}
+                            />
+                            <NumberButton
+                                value={6}
+                                onClick={() => handleButtonClick("6")}
+                            />
+                            <OperatorButton
+                                operation={"-"}
+                                onClick={() => handleButtonClick("-")}
+                            />
+                            <NumberButton
+                                value={1}
+                                onClick={() => handleButtonClick("1")}
+                            />
+                            <NumberButton
+                                value={2}
+                                onClick={() => handleButtonClick("2")}
+                            />
+                            <NumberButton
+                                value={3}
+                                onClick={() => handleButtonClick("3")}
+                            />
+                            <OperatorButton
+                                operation={"%"}
+                                onClick={() => handleButtonClick("%")}
+                            />
+                            <NumberButton
+                                value={0}
+                                onClick={() => handleButtonClick("0")}
+                            />
+                            <OperatorButton
+                                operation={"."}
+                                onClick={() => handleButtonClick(".")}
+                            />
+                            <button
+                                className="calculator__key calculator__key--enter"
+                                onClick={printResult}
+                            >
+                                =
+                            </button>
+                        </div>
                     </div>
                 </div>
             </header>
